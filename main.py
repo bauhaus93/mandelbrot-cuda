aa9d9011a1b72840e1943c535fd91901cc9dbad3
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-31 12:52:17 +0100

    Changed coloring

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from mandelbrot import Mandelbrot
 from utility import setup_logger
 
 if __name__ == "__main__":
-	# WINDOW_SIZE = (1920, 1080)
+	SNAPSHOT_SIZE = (1920, 1080)
 	WINDOW_SIZE = (800, 600)
 	INITIAL_DEPTH = 1000
 	setup_logger()
+	log = logging.getLogger()
 
-	mb = Mandelbrot(WINDOW_SIZE, INITIAL_DEPTH)
+	mb = Mandelbrot(INITIAL_DEPTH)
 	
 
 	pygame.init()
@@ -20,15 +21,37 @@ if __name__ == "__main__":
 
 	quit = False
 	needsUpdate = True
+	active_seq = None
 	while not quit:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit = True
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					pos = pygame.mouse.get_pos()
+					offset = (int(pos[0] - WINDOW_SIZE[0] / 2),
+						  int(pos[1] - WINDOW_SIZE[1] / 2))
+					mb.move(offset)
+					needsUpdate = True
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_e:
+				if event.key == pygame.K_F1:
+					mb.snapshot(SNAPSHOT_SIZE)
+				elif event.key == pygame.K_F2:
+					SEQ_COUNT = 100000
+					if active_seq is None:
+						log.info("Creating zoomed sequence...")
+						active_seq = mb.zoomed_sequence(SEQ_COUNT, WINDOW_SIZE, 1.01)
+				elif event.key == pygame.K_F3:
+					TRIES = 10000
+					if mb.find_poi(WINDOW_SIZE, max_tries = TRIES):
+						needsUpdate = True
+				elif event.key == pygame.K_F4:
+					mb.randomize_buckets()
+					needsUpdate = True
+				elif event.key == pygame.K_e:
 					mb.zoom(2.)
 					needsUpdate = True
-				elif event.key == pygame.K_r:
+				elif event.key == pygame.K_q:
 					mb.zoom(0.5)
 					needsUpdate = True
 				elif event.key == pygame.K_d:
@@ -37,19 +60,21 @@ if __name__ == "__main__":
 				elif event.key == pygame.K_f:
 					mb.mod_depth(-100)
 					needsUpdate = True
-				elif event.key == pygame.K_t:
-					mb.randomize_start_hue()
-					needsUpdate = True
-				elif event.key == pygame.K_g:
-					if mb.find_poi(4., 10000):
-						needsUpdate = True
+		if active_seq:
+			try:
+				progress = next(active_seq)
+				log.info(f"Progress: {progress}/{SEQ_COUNT}")
+			except StopIteration:
+				log.info("Finished sequence")
+				active_seq = None
+			needsUpdate = True
 
 		if needsUpdate:
 			needsUpdate = False
-			mb_img = mb.create_rgb().swapaxes(0, 1)
-			pygame.surfarray.blit_array(screen, mb_img)
+			mb.build_rgb(WINDOW_SIZE)
+			pygame.surfarray.blit_array(screen, mb.get_rgb().swapaxes(0, 1))
 			pygame.display.flip()
-
-		pygame.time.wait(100)
+		if not active_seq:
+			pygame.time.wait(100)
 
 
--- mandelbrot.py
+++ mandelbrot.py
@@ -10,6 +10,10 @@ from numba import jit
 
 from utility import setup_logger
 
+def default_filename():
+	ts = time.localtime()
+	return f"{ts.tm_year}-{ts.tm_mon:02}-{ts.tm_mday:02}_{ts.tm_hour:02}-{ts.tm_min:02}-{ts.tm_sec:02}"
+
 @jit
 def create_space(center, size, element_count):
 	lower = center - size / 2.
@@ -18,8 +22,8 @@ def create_space(center, size, element_count):
 
 @jit
 def create_complex_space(center, size, element_count):
-	return (create_space(center[0], size[0], element_count[0]),
-			create_space(center[1], size[1], element_count[1]))
+	return (create_space(center.real, size[0], element_count[0]),
+			create_space(center.imag, size[1], element_count[1]))
 @jit
 def mandelbrot_depth(c, max_depth):
 	z = c
@@ -30,66 +34,78 @@ def mandelbrot_depth(c, max_depth):
 	return 0
 
 @jit
-def create_depth_list(center, size, element_count, max_depth):
-	space_real, space_imag = create_complex_space (center, size, element_count)
-	depth_list = np.empty(element_count[0] * element_count[1], np.uint32)
-	for y in range(element_count[1]):
-		for x in range(element_count[0]):
-			depth_list[x + y * element_count[0]] =	mandelbrot_depth(space_real[x] + 1j * space_imag[y], max_depth)
+def create_depth_list(center, shape, shape_divider, max_depth):
+	size = (shape[0] / shape_divider, shape[1] / shape_divider)
+	space_real, space_imag = create_complex_space (center, size, shape)
+	depth_list = np.empty(shape[0] * shape[1], np.uint32)
+	for y in range(shape[1]):
+		for x in range(shape[0]):
+			depth_list[x + y * shape[0]] =	mandelbrot_depth(space_real[x] + 1j * space_imag[y], max_depth)
 	return depth_list
 
 @jit
-def create_mandelbrot(center, size, element_count, max_depth, loop_depth, start_hue):
-	space_real, space_imag = create_complex_space (center, size, element_count)
+def create_mandelbrot(center, shape, shape_divider, max_depth, buckets):
+	size = (shape[0] / shape_divider, shape[1] / shape_divider)
+	space_real, space_imag = create_complex_space (center, size, shape)
 
-	depth_list = create_depth_list(center, size, element_count, max_depth)
-	depth_nonzero = depth_list[np.nonzero(depth_list)]
-	if len(depth_nonzero) > 0:
-		min_depth = np.amin(depth_nonzero)
-	else:
-		min_depth = 0
-	max_depth = np.amax(depth_list)
+	depth_list = create_depth_list(center, shape, shape_divider, max_depth)
 
-	hsv_img = np.empty((element_count[1], element_count[0], 3), np.uint8)
-	for y in range(element_count[1]):
-		for x in range(element_count[0]):
-			depth =	 depth_list[x + y * element_count[0]]
-			hsv_img[y,x] = assign_hsv_color(depth, min_depth, max_depth, start_hue)
+	hsv_img = np.empty((shape[1], shape[0], 3), np.uint8)
+	for y in range(shape[1]):
+		for x in range(shape[0]):
+			depth =	 depth_list[x + y * shape[0]]
+			hsv_img[y,x] = assign_hsv_color(depth, buckets)
 	return hsv_img
 
 @jit
-def assign_hsv_color(depth, min_depth, max_depth, start_hue):
+def assign_hsv_color(depth, buckets):
 	if depth == 0:
-		hue = 0
-		sat = 0
-		val = 0
+		col = np.zeros(3, np.uint8)
 	else:
-		factor = (depth - min_depth) / (max_depth - min_depth)
-		# factor = 1. - 10**(-depth / max_depth)
-		# factor = max(1e-3, min(1., factor))
-		hue = (start_hue + int(180. * factor)) % 180
-		sat = 255
-		val = 255 
-	return np.array([hue, sat, val], np.uint8)
+		col = buckets[depth % buckets.shape[0]]
+	return col
+
+def create_hsv_buckets(amount):
+	buckets = np.empty((amount, 3), np.uint8)
+	acc = [0.] * 3
+	direction = [random.randint(-1, 1)] * 3
+	for i in range(amount):
+		acc = [a  + random.uniform(0., 0.10) for a in acc]
+		for j in range(len(direction)):
+			if acc[j] >= random.random():
+				acc[j] = 0.
+				direction[j] = random.randint(-1, 1)
+
+		if i == 0:
+			hue = random.randint(0, 180)
+			sat = random.randint(0, 255)
+			val = random.randint(0, 255)
+		else:
+			step = random.randint(1, 8)
+			hue = min(180, max(0, buckets[i-1][0] + direction[0] * step))
+			sat = min(255, max(50, buckets[i-1][1] + direction[1] * step))
+			val = min(255, max(50, buckets[i-1][2] + direction[2] * step))
+		buckets[i] = (hue, sat, val)
+	return buckets
 
 log = logging.getLogger()
 
 class Mandelbrot:
 
-	center = (0.41825763942621236, -0.34087020388354944)
+	center = 0.41825763942621236 - 0.34087020388354944j
 	shape_divider = 1e12
-	start_hue = 0
-	def __init__(self, shape, max_depth):
-		self.size = (shape[0] / self.shape_divider, shape[1] / self.shape_divider)
-		self.shape = shape
+	last_img = None
+	def __init__(self, max_depth):
 		self.max_depth = max_depth
-		self.loop_depth = max_depth / 10
+		self.randomize_buckets()
 
 	def randomize_center(self):
-		self.center = (random.uniform(-2., 2.), random.uniform(-2., 2.))
+		self.center = random.uniform(-2., 2.) + 1j * random.uniform(-2., 2.)
 
-	def randomize_start_hue(self):
-		self.start_hue = random.randint(0, 180)
+	def randomize_buckets(self, amount = None):
+		if amount is None:
+			amount = self.max_depth
+		self.buckets = create_hsv_buckets(amount)
 	
 	def randomize_zoom(self):
 		self.shape_divider = random.uniform(1e-5, 1e-14)
@@ -105,43 +121,63 @@ class Mandelbrot:
 
 	def zoom(self, factor):
 		self.shape_divider *= factor
-		self.size = (self.shape[0] / self.shape_divider, self.shape[1] / self.shape_divider)
+	
+	def move(self, units):
+		self.center = (self.center[0] + units[0] / self.shape_divider,
+			       self.center[1] + units[1] / self.shape_divider)
 
-	def estimate_entropy(self):
-		depth_list = create_depth_list(self.center, self.size, (50, 50), self.max_depth)
+	def estimate_entropy(self, shape, points = 50):
+		fact = max(shape[0] / points, shape[1] / points)
+		depth_list = create_depth_list(self.center, (50, 50), self.shape_divider * fact, self.max_depth)
 		_val, counts = np.unique(depth_list, return_counts = True)
 		return scipy.stats.entropy(counts)
 
-	def find_poi(self, min_entropy = 4., max_tries = 1000):
-		log.info("Searching POI...")
+	def find_poi(self, shape, min_entropy = 4., max_tries = 1000):
+		log.info(f"Searching POI...")
 		tries = 0
+		old_center = self.center
+		max_entropy = 0.
 		while tries < max_tries:
 			tries += 1
 			self.randomize_center()
-			entropy = self.estimate_entropy()
+			entropy = self.estimate_entropy(shape)
 			if entropy >= min_entropy:
-				log.info(f"Found POI @ {self.center[0] + 1j * self.center[1]}, tries = {tries}, entropy = {entropy}")
+				log.info(f"Found POI @ {self.center}, tries = {tries}, entropy = {entropy}")
 				return True
+			elif entropy > max_entropy:
+				max_entropy = entropy
+		log.info(f"No POI found after {max_tries} tries, max entropy found was {max_entropy}")
+		self.center = old_center
 		return False
 
+	def zoomed_sequence(self, count, shape, zoom_factor):
+		for i in range(count):
+			self.snapshot(shape, f"seq_{i:05d}")
+			self.zoom(zoom_factor)
+			if i % 10 == 0:
+				yield i
+		yield 
 
-	def random_snapshot(self, min_entropy, max_tries):
-		while not self.find_poi(min_entropy, max_tries):
-			self.randomize_start_hue()
+	def random_snapshot(self, shape, min_entropy, max_tries, name = default_filename()):
+		while not self.find_poi(shape, min_entropy, max_tries):
 			self.snapshot()
 
-	def snapshot(self):
+	def snapshot(self, shape, name = default_filename()):
+		self.build_rgb(shape)
 		ts = time.localtime()
-		rgb_img = self.create_rgb()
-		return cv2.imwrite(f"{ts.tm_year}-{ts.tm_mon:02}-{ts.tm_mday:02}_{ts.tm_hour:02}-{ts.tm_min:02}-{ts.tm_sec:02}.png", rgb_img)
+		return cv2.imwrite(name + ".png", self.last_img)
 	
-	def create_rgb(self):
-		log.info(f"Computing mandelbrot: shape = {self.shape[0]} x {self.shape[1]}, max_depth = {self.max_depth}")
+	def build_rgb(self, shape):
+		log.info(f"Computing mandelbrot: shape = {shape[0]} x {shape[1]}, max_depth = {self.max_depth}")
+		log.info(f"Center: {self.center}")
 		start = time.time()
-		hsv_img = create_mandelbrot(self.center, self.size, self.shape, self.max_depth, self.loop_depth, self.start_hue)
+		hsv_img = create_mandelbrot(self.center, shape, self.shape_divider, self.max_depth, self.buckets)
 		diff = time.time() - start
 		log.info("Computation took {0:.2f}s".format(diff))
-		return cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+		self.last_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+
+	def get_rgb(self):
+		return self.last_img
 
 
 if __name__ == "__main__":
@@ -149,8 +185,9 @@ if __name__ == "__main__":
 	MAX_DEPTH = 1500
 
 	setup_logger()
+	# random.seed("HELLO")
 
-	mb = Mandelbrot(SHAPE, MAX_DEPTH)
+	mb = Mandelbrot(MAX_DEPTH)
 	for i in range(1000):
 		mb.randomize_zoom()
 		mb.randomize_depth()
